8e58533dbd0f1b7d5aba3fc4d1fe0aa27bf6d737
User <user@MacBook-Pro-de-User.local>
2024-08-20 18:07:31 -0300

    first commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import Porta from "../../components/Porta";
+import Presente from "../../components/Presente";
+
+export default function main() {
+  return (
+    <div>
+      <Porta />
+      <Porta />
+    </div>
+  );
+}
